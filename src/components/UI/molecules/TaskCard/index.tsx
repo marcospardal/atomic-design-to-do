5b97445930535fce2a8d15b226cf848537db6fd0
Pardal <marcosnascimento@eng.ci.ufpb.br>
2024-08-24 05:08:52 -0300

    add animations task card

--- src/components/UI/molecules/TaskCard/index.tsx
+++ src/components/UI/molecules/TaskCard/index.tsx
@@ -1,5 +1,5 @@
 import Task from '@/types/Task.types';
-import React from 'react';
+import React, { useRef } from 'react';
 import { Label } from '../../atoms';
 import './styles.css';
 import Info from '../../atoms/Info';
@@ -9,16 +9,20 @@ interface TaskCardProps extends Task { }
 
 export default function TaskCard({ id, title, description, isCompleted }: TaskCardProps) {
   const { finishToDo } = useToDoContext();
+  const cardRef = useRef<HTMLDivElement | null>(null);
+
+  function handleFinish() {
+    cardRef.current?.setAttribute('class', 'task-card fade-out');
+    setTimeout(() => {
+      finishToDo(id);
+    }, 400);
+  }
 
   return (
-    <div key={id} className='task-card'>
+    <div key={id} ref={cardRef} className='task-card'>
       <div className='task-card-header'>
         <Label label={title} />
-        {!isCompleted && (
-          <>
-            <input type='checkbox' disabled={isCompleted} onClick={() => finishToDo(id)} />
-          </>
-        )}
+        {!isCompleted && <input type='checkbox' disabled={isCompleted} onClick={handleFinish} />}
       </div>
       <Info text={description} />
     </div>
